d33abe8039038bddc9394707459d5322d13496d4
Bùi Hoàng Minh <buihoangminhsmbrn07@gmail.com>
2024-08-20 12:48:34 +0700

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,6 @@
 set fish_greeting ""
 
-set -gx TERM xterm-256color
-
 # theme
-set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_user yes
 set -g theme_hide_hostname no
@@ -23,21 +20,11 @@ set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 
-# NodeJS
-set -gx PATH node_modules/.bin $PATH
-
 # Go
 set -g GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
 
-switch (uname)
-    case Darwin
-        source (dirname (status --current-filename))/config-osx.fish
-    case Linux
-        source (dirname (status --current-filename))/config-linux.fish
-    case '*'
-        source (dirname (status --current-filename))/config-windows.fish
-end
+source (dirname (status --current-filename))/config-linux.fish
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
